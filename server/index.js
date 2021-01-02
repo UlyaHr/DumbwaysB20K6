29f4b1e95a3e3726faa6cd13fbc6fdb851d22073
UlyaHr <ulya.habiburrahman@mail.ugm.ac.id>
2021-01-02 21:07:38 +0700

    Backend Route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(3001, (req, res) => {
-  console.log("Server running...");
+const uploadRoute = require('./routes/Upload')
+app.use('/upload', uploadRoute);
+
+app.listen(3002, (req, res) => {
+  console.log("Server running..");
 });
---
+++ server/routes/Upload.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const db = require("../config/db");
+
+router.get("/", (req, res) => {
+  db.query("SELECT * FROM hero", (err, results) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(results);
+  });
+});
+
+module.exports = router;
